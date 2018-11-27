d5a96ed8c4fd25b2148769677ea0426ada38a79e
timmoffett <timothytmoffett@gmail.com>
2018-11-27 10:27:16 -0700

    changed port for class requirements

--- main.py
+++ main.py
@@ -32,4 +32,4 @@ def handle_event(json, methods=['GET', 'POST']):
     socketio.emit('response', json, callback=messageReceived)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=9020, debug=True)

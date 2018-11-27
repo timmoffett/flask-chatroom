5bce0f8b4676bd348e835746fade189556ab026d
timmoffett <timothytmoffett@gmail.com>
2018-11-27 11:08:11 -0700

    fixed login, username immutable. Need to redirect root file to login if not logged in

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from flask import Flask, render_template, request, redirect, session, url_for
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-app.config['SECRET KEY'] = '123rhymewithme'
+app.secret_key = 'dumbkey'
+app.config['SESSION_TYPE'] = 'memcached' 
 socketio = SocketIO(app)
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -12,12 +13,13 @@ def login():
     error = None
     if request.method == 'POST':
         session['username'] = request.form['username']
-        return redirect(url_for('home'))
-    return render_template('login.html', error=error)
+        return redirect(url_for('sessions'))
+    else:
+        return render_template('login.html', error=error)
 
 @app.route('/')
 def sessions():
-    username = session.get('Username', None)
+    username = session['username']
     return render_template('chatroom.html', user=username)
 
 def messageReceived(methods=['GET', 'POST']):

4e54647ddf41f4d626b9ce812f20df40ce90cddc
timmoffett <timothytmoffett@gmail.com>
2018-11-27 10:08:53 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python
+
+from flask import Flask, render_template, request, redirect, session, url_for
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET KEY'] = '123rhymewithme'
+socketio = SocketIO(app)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+@app.route('/')
+def sessions():
+    username = session.get('Username', None)
+    return render_template('chatroom.html', user=username)
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message received')
+
+@socketio.on('my event')
+def handle_event(json, methods=['GET', 'POST']):
+    print('received event: ' + str(json))
+    print type(json)
+    if json['message'] == 'help':
+        print('worked')
+    socketio.emit('response', json, callback=messageReceived)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
